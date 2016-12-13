a493e4935992687f8fd3213eefc24d6d7107a312
mkubota1111 <marcus.kubota@gmail.com>
2016-12-13 14:41:53 -0500

    7.1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent }  from './app.component';
@@ -9,12 +14,12 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 
-import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
